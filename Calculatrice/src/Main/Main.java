82f0a1536b3212025f7c8bb5d51fb6ee5db340c5
mathi <mathi@159.31.67.253>
2023-10-18 13:58:13 +0200

    Ajout projets Calc

---
+++ Calculatrice/src/Main/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+import java.util.Stack;
+
+public class Main 
+{
+	public static void main(String [] args) 
+	{
+		Calcul c = new Calcul();
+		
+		Stack<Double> calculatrice = new Stack<Double>();
+		c.push(1);
+		c.push(2);
+		c.add();
+
+	}
+}
+
+
