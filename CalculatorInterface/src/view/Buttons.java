af4c2fc621199c0417c245aa4b17a74ad01c2eec
lucie <lucie@LAPTOP-169NQGCV>
2023-10-24 10:05:32 +0200

    modif1

--- CalculatorInterface/src/view/Buttons.java
+++ CalculatorInterface/src/view/Buttons.java
@@ -14,7 +14,8 @@ import java.util.Stack;
 
 public class Buttons{
 	
-	static TextField resultat;
+	static TextField resultat = new TextField();
+
 	
 	/**
 	 * 
@@ -24,6 +25,7 @@ public class Buttons{
 	public static Button makeButton(String label) {
 	    Button button = new Button(label);
 	    button.setMinSize(90, 40);
+        button.setOnAction(event -> handleButtonClick(label));
 	    return button;
 	}
 	
@@ -32,15 +34,13 @@ public class Buttons{
 	 * @return
 	 */
 	 public static GridPane createButtonGrid() {
-		 
-		    Buttons.resultat = resultat; 
+		  
 	        GridPane grid = new GridPane();
 	        grid.setPadding(new Insets(10));
 			grid.setHgap(5);
 		    grid.setVgap(5);
 		    
 
-		    TextField resultat = new TextField();
 		    resultat.setStyle("-fx-alignment: CENTER-RIGHT;");
 		    resultat.setPrefColumnCount(4);
 		    resultat.setEditable(false); 
@@ -60,7 +60,7 @@ public class Buttons{
 		    for (String label : buttonLabels) {
 		        Button button = makeButton(label);
 		        grid.add(button, colonne, ligne);
-		        button.setOnAction(event -> CalculatorGUI.handleButtonClick(label));
+
 		        colonne++;
 		        if (colonne > 3) {
 		            colonne = 0;
@@ -68,5 +68,54 @@ public class Buttons{
 		        }}
 		    return grid;
 		    }
-	
+	 
+	 
+		
+	public static void handleButtonClick(String label) {
+		
+		if (label.equals("1")) {
+			resultat.setText("1");
+		} else {
+			resultat.appendText(label);
+		}
+////	    if (label.equals("push")) {
+////	        // Si l'utilisateur appuie sur "push", ajoutez le nombre saisi à la pile
+////	        try {
+////	            double num = Double.parseDouble(Buttons.resultat.getText()); // Utilisez la variable statique de Buttons
+////	            stack.push(num); 
+////	            Buttons.resultat.clear(); 
+//	        } catch (NumberFormatException e) {
+//	            Buttons.resultat.setText("Erreur"); 
+//	        }
+//	    } else if (label.equals("+") || label.equals("-") || label.equals("*") || label.equals("/")) {
+//	     
+//	        if (stack.size() >= 2) {
+//	            double num2 = stack.pop();
+//	            double num1 = stack.pop();
+//	            double result = 0.0;
+//	            if (label.equals("+")) {
+//	                result = num1 + num2;
+//	            } else if (label.equals("-")) {
+//	                result = num1 - num2;
+//	            } else if (label.equals("*")) {
+//	                result = num1 * num2;
+//	            } else if (label.equals("/")) {
+//	                if (num2 != 0) {
+//	                    result = num1 / num2;
+//	                } else {
+//	                    Buttons.resultat.setText("Erreur"); // Utilisez la variable statique de Buttons
+//	                    return;
+//	                }
+//	            }
+//	            stack.push(result); // Utilisez la variable statique de Buttons
+//	            Buttons.resultat.clear(); // Utilisez la variable statique de Buttons
+//	            Buttons.resultat.setText(Double.toString(result)); // Utilisez la variable statique de Buttons
+//	        } else {
+//	            Buttons.resultat.setText("Erreur"); // Utilisez la variable statique de Buttons
+//	        }
+//	    } else {
+//	        // Si l'utilisateur appuie sur un chiffre ou un point, ajoutez-le au champ de texte
+//	        Buttons.resultat.appendText(label); // Utilisez la variable statique de Buttons
+//	    }
+	}
 }
